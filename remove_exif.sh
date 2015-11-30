65544f8f07f8931df25b51dfbab601aed36ffe02
Michel Wohlert @XMG-Arch <michel.wohlert@zmmail.de>
2015-11-30 18:59:33 +0100

    Add Content

---
+++ remove_exif.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+unexif(){
+	tempfile=$1"/*"
+	echo "$tempfile"
+	for f in $tempfile
+	do
+		if [ -d "$f" ] ; then
+			unexif "$f"
+		else
+			tempdata=$f".tmp"
+			mv "$f" "$tempdata"
+			exiftool -AllDates="" -all="" -overwrite_original_in_place "$tempdata"
+			type="$(file -b $tempdata)"
+			if [ "${type%%,*}" == "PDF document" ]; then
+				qpdf --linearize "$tempdata" "$f"
+  				echo "$f is a PDF file. Deleting orphan stuff to prevend reversing changes"
+			fi
+  			echo "Processing $f"
+  			rm "$tempdata"
+  		fi
+	done
+}
+
+#Check if user is root
+if [[ $EUID -ne 0 ]]; then
+   echo -e "\e[1m\e[31mThis script must be run as root\e[39m\e[21m" 1>&2
+   exit 1
+fi
+
+systemctl stop systemd-timesyncd
+date +%Y%m%d -s "19700102" >/dev/null 2>&1
+
+
+FILES=unexif/*
+for f in $FILES
+do
+	if [ -d "$f" ] ; then
+		unexif "$f"
+	else
+		tempdata=$f".tmp"
+		mv "$f" "$tempdata"
+		exiftool -AllDates="" -all="" -overwrite_original_in_place "$tempdata"
+		type="$(file -b $tempdata)"
+		if [ "${type%%,*}" == "PDF document" ]; then
+			qpdf --linearize "$tempdata" "$f"
+  			echo "$f is a PDF file. Deleting orphan stuff to prevend reversing changes"
+		fi
+  		echo "Processing $f"
+  		rm "$tempdata"
+  	fi
+done
+
+
+systemctl start systemd-timesyncd
+IFS=$SAVEIFS
